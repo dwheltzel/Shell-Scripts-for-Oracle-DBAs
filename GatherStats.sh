f4f7c757781848a3f20914f5dfded8e042cfd963
Dennis Heltzel <dwheltzel@users.noreply.github.com>
2021-10-09 09:05:01 -0400

    Update GatherStats.sh

--- GatherStats.sh
+++ GatherStats.sh
@@ -1,12 +1,18 @@
-# Gather table stats based on time and size (number of records)
-#
-# written by Dennis Heltzel
+# GatherStats.sh - Gather table stats based on time and size (number of records)
+# 
+# Author: Dennis Heltzel
+
+. /home/oracle/bin/ora_funcs.sh
+ORACLE_BASE=/u01/app/oracle
+ORACLE_HOME=${ORACLE_BASE}/product/12.1.0.2/DbHome_2
+PATH=$PATH:$ORACLE_HOME/bin
 
 CRED=${CRED:-/}
 
 usage() {
   echo "Usage: $0 [-t days] [-r num_recs] [-d database name] [-s] [-f filter_string]"
   echo "  -t days - only select tables that have not had stats gathered in at least this many days (default 7)"
+  echo "  -p days - only select partitions that have not had stats gathered in at least this many days (default 180)"
   echo "  -r num_recs - only select tables with fewer than X thousand records (default is unlimited)"
   echo "  -d database name - defaults to $ORACLE_SID"
   echo "  -s - also gather system stats (takes a long time)"
@@ -15,14 +21,16 @@ usage() {
 }
 
 # Handle parameters
-while getopts ":d:t:r:sf:" opt; do
+while getopts ":d:t:p:r:sf:" opt; do
   case $opt in
     d)
-      TWO_TASK=$OPTARG
       DB_NAME=$OPTARG
       ;;
     t)
       AGE_DAYS=$OPTARG
+      ;;
+    p)
+      PART_AGE_DAYS=$OPTARG
       ;;
     r)
       NUM_RECS="AND t.num_rows < ($OPTARG * 1000) "
@@ -44,41 +52,62 @@ while getopts ":d:t:r:sf:" opt; do
   esac
 done
 
+oe ${DB_NAME}
 DB_NAME=${DB_NAME:-${ORACLE_SID}}
+#echo "DB_NAME=${DB_NAME}:ORACLE_SID=${ORACLE_SID}:"
+
 AGE_DAYS=${AGE_DAYS:-7}
-REPORT_NAME=GatherStats_${DB_NAME}-`date "+%y%m%d%H%M"`.sql
-LOG_NAME=GatherStats_${DB_NAME}-`date "+%y%m%d%H%M"`.log
+PART_AGE_DAYS=${PART_AGE_DAYS:-180}
+LOG_DIR=/cloudfs/logs
+BASE_NAME=${LOG_DIR}/GatherStats_${DB_NAME}
+SQL_NAME=${BASE_NAME}.sql
+LOG_FILE=${BASE_NAME}.log
 #echo "${AGE_DAYS}"
 #echo "${NUM_RECS}"
-#echo "${REPORT_NAME}"
+#echo "${SQL_NAME}"
 #echo "${OPT_FILTER}"
+
+:> ${LOG_FILE}
+exec >> $LOG_FILE 2>&1
+
 echo "Gathering stats for tables in ${DB_NAME} with stats older that ${AGE_DAYS} days . . ."
 
 (sqlplus -s ${CRED} as sysdba <<!
 set pages 0
 set feed off
 set head off
-set lines 130
+set lines 180
 prompt set sqlprompt "_CONNECT_IDENTIFIER> ";
 prompt set feed off;
 prompt set echo on;
---prompt spool ${LOG_NAME}
-SELECT 'exec DBMS_STATS.GATHER_TABLE_STATS('''||t.owner||''','''||t.table_name||''');' FROM dba_tables t
+prompt set time on;
+prompt set timi on;
+prompt spool ${LOG_FILE}
+SELECT 'exec DBMS_STATS.GATHER_TABLE_STATS('''||t.owner||''',''"'||t.table_name||'"'',CASCADE=>TRUE);' FROM dba_tables t
  JOIN dba_tab_statistics s ON (s.OWNER = t.OWNER AND s.TABLE_NAME = t.TABLE_NAME)
- WHERE t.owner NOT LIKE '%SYS%' AND t.owner NOT IN ('XDB') AND t.temporary = 'N' AND t.secondary = 'N' AND t.segment_created = 'YES'
+ WHERE t.owner IN (SELECT username FROM dba_users WHERE profile = 'DATACCT') AND t.temporary = 'N' AND t.secondary = 'N' AND t.segment_created = 'YES'
  AND (t.owner,t.table_name) NOT IN (SELECT owner, table_name FROM dba_external_tables)
  AND s.stattype_locked IS NULL AND (t.last_analyzed < SYSDATE -  ${AGE_DAYS} OR t.last_analyzed IS NULL) AND t.table_name NOT LIKE 'SYS%' ${NUM_RECS} ${OPT_FILTER}
  ORDER BY t.num_rows;
+SELECT 'exec DBMS_STATS.GATHER_TABLE_STATS('''||t.table_owner||''',''"'||t.table_name||'"'','''||t.partition_name||''',granularity=>''APPROX_GLOBAL AND PARTITION'',CASCADE=>TRUE);' FROM dba_tab_partitions t
+ JOIN dba_tab_statistics s ON (s.OWNER = t.table_owner AND s.TABLE_NAME = t.TABLE_NAME AND s.partition_name = t.partition_name)
+ WHERE t.table_owner IN (SELECT username FROM dba_users WHERE profile = 'DATACCT') AND t.segment_created = 'YES'
+ AND (t.table_owner,t.table_name) NOT IN (SELECT owner, table_name FROM dba_external_tables)
+ AND s.stattype_locked IS NULL
+ AND t.last_analyzed IS NULL
+-- AND ((t.last_analyzed < SYSDATE - ${PART_AGE_DAYS}) OR t.last_analyzed IS NULL)
+ AND t.table_name NOT LIKE 'SYS%' ${NUM_RECS} ${OPT_FILTER}
+ ORDER BY t.num_rows;
 exit
 !
-) > ${REPORT_NAME}
+) > ${SQL_NAME}
 
 # Add commands to gather system stats if requested
 if [ -n "${SYS_STATS}" ] ; then
-  echo "exec DBMS_STATS.GATHER_SYSTEM_STATS" >>${REPORT_NAME}
-  echo "exec DBMS_STATS.GATHER_DICTIONARY_STATS" >>${REPORT_NAME}
-  echo "exec DBMS_STATS.GATHER_FIXED_OBJECTS_STATS" >>${REPORT_NAME}
+  echo "exec DBMS_STATS.GATHER_SYSTEM_STATS" >>${SQL_NAME}
+  echo "exec DBMS_STATS.GATHER_DICTIONARY_STATS" >>${SQL_NAME}
+  echo "exec DBMS_STATS.GATHER_FIXED_OBJECTS_STATS" >>${SQL_NAME}
 fi
-echo "exit" >>${REPORT_NAME}
+echo "exit" >>${SQL_NAME}
 
-sqlplus ${CRED} as sysdba @${REPORT_NAME}
+sqlplus ${CRED} as sysdba @${SQL_NAME}
