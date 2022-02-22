c24fe596f41185879e60bebd20895dab089cb90c
Dennis Heltzel <dwheltzel@users.noreply.github.com>
2022-02-22 12:17:48 -0500

    Add files via upload

--- RMAN_backup.sh
+++ RMAN_backup.sh
@@ -1,16 +1,17 @@
-# rman_full_bkp.sh - Take rman full backup(incremental level 0 )
+# RMAN_backup.sh - Take rman full backup(incremental level 0 )
 # 
 # Author: Dennis Heltzel 02/9/2022
 
 LOGDIR=/home/oracle/logs
 
 usage() {
-  echo "Usage: $0 [-d db name] [-p PDB name] [-i incremental level] [-t tag name] [-l log directory] [-c] [-s]"
+  echo "Usage: $0 [-d db name] [-p PDB name] [-i incremental level] [-t tag name] [-l log directory] [-e] [-c] [-s]"
   echo "  -d container database name - defaults to ${ORACLE_SID}"
   echo "  -p PDB to backup - defaults to entire container"
   echo "  -l incremental level - if left off, a full, non-incremental backup is performed"
   echo "  -t tag name"
   echo "  -l log directory - defaults to ${LOGDIR}"
+  echo "  -e - only cleanup expired or missing files (always done before any backup)"
   echo "  -c - prints the RMAN configuration and exits"
   echo "  -s - sets the RMAN configuration and exits"
   exit 1
@@ -21,15 +22,17 @@ PDBNAME=${ORACLE_PDB_SID}
 CONN_STR='sqlplus -s / as sysdba'
 RUNDIR=`dirname "${BASH_SOURCE[0]}"`
 . ${RUNDIR}/ora_funcs.sh
+export PATH=$PATH:$ORACLE_HOME/bin
 LEVEL=
 TAGNAME=
+CROSSCHECK_ONLY=
 SHOW_CFG=
 SET_CFG=
 #START_DATE=`date '+%Y%m%d%H%M'`
 RMAN_DATE=$(date +%y-%m-%d_%H%M%S)
 
 # Handle parameters
-while getopts "d:p:i:t:l:cs" opt; do
+while getopts "d:p:i:t:l:ecs" opt; do
   case $opt in
     d)
       CDBNAME=$OPTARG
@@ -45,6 +48,9 @@ while getopts "d:p:i:t:l:cs" opt; do
       ;;
     l)
       LOGDIR=$OPTARG
+      ;;
+    e)
+      CROSSCHECK_ONLY=YES
       ;;
     c)
       SHOW_CFG=YES
@@ -63,6 +69,7 @@ while getopts "d:p:i:t:l:cs" opt; do
   esac
 done
 
+oe ${CDBNAME}
 # Set tagname
 if [ -z ${TAGNAME} ] ; then
   if [ -z ${LEVEL} ] ; then
@@ -81,11 +88,11 @@ LOGFILE=${LOGDIR}/RMAN_${TAGNAME}.log
 #export PATH=$ORACLE_HOME/bin:$PATH
 
 showConfig () {
-  rman << EOF
+  rman <<!
 connect target /
 show all;
 exit
-EOF
+!
 }
 
 #SELECT OPERATION, STATUS, MBYTES_PROCESSED, START_TIME, END_TIME from Vi\$RMAN_STATUS;
@@ -93,7 +100,7 @@ EOF
 #configure controlfile autobackup format for device type disk to '$RMANBACKUP_MOUNTPOINT1/%F';
 #configure device type disk parallelism $PARALLELISM;
 setConfig () {
-  rman << EOF
+  rman <<!
 connect target /
 set echo on;
 configure retention policy to recovery window of 7 days;
@@ -102,38 +109,41 @@ configure controlfile autobackup on;
 configure maxsetsize to unlimited;
 show all;
 exit
-EOF
+!
 }
 
 crosscheck () {
-  rman log= ${LOGDIR}/RMAN_${TAGNAME}-delete.log << EOF
+  (rman <<!
 connect target /
 set echo on;
 crosscheck archivelog all;
 crosscheck backup;
 delete noprompt obsolete;
 exit
-EOF
+!
+) > ${LOGDIR}/RMAN_${TAGNAME}-delete.log
 }
 
 incrLevel () {
-  rman log=${LOGFILE} << EOF
+  (rman <<!
 connect target /
 set echo on;
 backup as compressed backupset tag '${TAGNAME}' incremental level ${LEVEL} database plus archivelog;
 sql 'alter system archive log current';
 exit
-EOF
+!
+) > ${LOGFILE}
 }
 
 full_backup () {
-  rman log=${LOGFILE} << EOF
+  (rman <<!
 connect target /
 set echo on;
 backup as compressed backupset tag '${TAGNAME}' database plus archivelog;
 sql 'alter system archive log current';
 exit
-EOF
+!
+) > ${LOGFILE}
 }
 
 # Main logic
@@ -147,14 +157,21 @@ if [ "${SET_CFG}" == 'YES' ] ; then
   setConfig
   exit 0
 fi
+# If CROSSCHECK_ONLY=YES, exit afterwards
+if [ "${CROSSCHECK_ONLY}" == 'YES' ] ; then
+  crosscheck
+  exit 0
+fi
 
 # Crosscheck and purge any obsolete backup pieces - this prevents error messages later
+tty -s && echo "Performing crosscheck and deleting missing or obsolete backup pieces"
 crosscheck
-exit
 
 # Perform actual backup
-if [ -z ${LEVEL} ] ;
+if [ -z ${LEVEL} ] ; then
+  tty -s && echo "Performing a full backup . . ."
   full_backup
 else
+  tty -s && echo "Performing an incremental ${LEVEL} backup . . ."
   incrLevel ${LEVEL}
 fi
